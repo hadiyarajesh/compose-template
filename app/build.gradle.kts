a3a58a2dcd753b47a7f1a1a3debe7a4c588703f6
Rajesh Hadiya <hadiarajesh007@gmail.com>
2022-08-13 19:30:48 +0530

    1. Bumped compose version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 object LibVersion {
-    const val composeVersion = "1.2.0-rc03"
+    const val composeVersion = "1.2.0"
     const val composeCompilerVersion = "1.2.0"
     const val retrofitVersion = "2.9.0"
     const val moshiVersion = "1.13.0"
@@ -60,12 +60,12 @@ object LibVersion {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.activity:activity-compose:1.5.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.compose.ui:ui:${LibVersion.composeVersion}")
     implementation("androidx.compose.material:material:${LibVersion.composeVersion}")
     implementation("androidx.compose.ui:ui-tooling-preview:${LibVersion.composeVersion}")
-    implementation("androidx.navigation:navigation-compose:2.5.0")
+    implementation("androidx.navigation:navigation-compose:2.5.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("com.google.dagger:hilt-android:${rootProject.extra["hiltVersion"]}")
     kapt("com.google.dagger:hilt-android-compiler:${rootProject.extra["hiltVersion"]}")
