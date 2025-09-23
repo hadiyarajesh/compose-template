d09113d48de56b3f61c06ce6ceca737b48d6ccaa
hadiyarajesh <rajesh.hadiya@outlook.com>
2025-09-23 19:11:29 +0530

    upgrade libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "com.hadiyarajesh.composetemplate"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
