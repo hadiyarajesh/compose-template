4594ec35035cf8fbeeab1539a165a955b3a974e4
rajesh-personatech <hadiarajesh007@gmail.com>
2026-01-06 15:42:04 +0530

    upgrade dependencies

--- app/src/main/java/com/hadiyarajesh/composetemplate/ui/home/HomeScreen.kt
+++ app/src/main/java/com/hadiyarajesh/composetemplate/ui/home/HomeScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.hadiyarajesh.composetemplate.R
